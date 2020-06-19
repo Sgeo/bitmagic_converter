10ddee2e470ddf11e8fbd8a6aa21ae7d695736f1
Sgeo <sgeoster@gmail.com>
2020-06-19 00:22:44 -0400

    Support for other extensions (as long as they're 3 bytes)

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,12 @@ fn main() {
     let mut input_file = File::open(&filename).expect("Unable to open file!");
     input_file.seek(SeekFrom::Start(4)).expect("Unable to seek!");
     let header_size = input_file.read_u32::<BigEndian>().expect("Unable to read BitMagic header size!");
+    input_file.seek(SeekFrom::Start(0x1C)).expect("Unable to seek!");
+    let mut ext_bytes: [u8; 3] = [0, 0, 0];
+    input_file.read_exact(&mut ext_bytes).expect("Unable to read extension!");
+    let ext = std::str::from_utf8(&ext_bytes).expect("Unable to convert extension to UTF-8!");
     input_file.seek(SeekFrom::Start(header_size.into())).expect("Unable to seek!");
-    let mut output_file = File::create(format!("{}.swf", &filename)).expect("Unable to create output file!");
+    let mut output_file = File::create(format!("{}.{}", &filename, ext)).expect("Unable to create output file!");
     std::io::copy(&mut input_file, &mut output_file).expect("Unable to copy data!");
 
 }
