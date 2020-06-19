6816cb54a3ec96c7f9a271f89545d14c0090c354
Sgeo <sgeoster@gmail.com>
2020-06-19 01:10:19 -0400

    Metadata file as second argument.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,31 @@ use std::io::prelude::*;
 
 use byteorder::{ReadBytesExt, WriteBytesExt, BigEndian, LittleEndian};
 
+fn read_bm_string<T: Read>(file: &mut T) -> String {
+    let size = file.read_u32::<BigEndian>().expect("Unable to read string size!");
+    let mut bytes: Vec<u8> = vec![0; size as usize];
+    file.read_exact(bytes.as_mut_slice()).expect("Unable to read a string!");
+    String::from_utf8_lossy(&bytes).into_owned()
+}
+
 
 fn main() {
     let filename = std::env::args().nth(1).expect("Please provide a filename!");
+    let metadata_filename_opt = std::env::args().nth(2);
     let mut input_file = File::open(&filename).expect("Unable to open file!");
     input_file.seek(SeekFrom::Start(4)).expect("Unable to seek!");
     let header_size = input_file.read_u32::<BigEndian>().expect("Unable to read BitMagic header size!");
-    input_file.seek(SeekFrom::Start(0x1C)).expect("Unable to seek!");
-    let mut ext_bytes: [u8; 3] = [0, 0, 0];
-    input_file.read_exact(&mut ext_bytes).expect("Unable to read extension!");
-    let ext = std::str::from_utf8(&ext_bytes).expect("Unable to convert extension to UTF-8!");
+    input_file.seek(SeekFrom::Start(0x18)).expect("Unable to seek!");
+    let ext = read_bm_string(&mut input_file);
+    let name = read_bm_string(&mut input_file);
+    let creator = read_bm_string(&mut input_file);
+
     input_file.seek(SeekFrom::Start(header_size.into())).expect("Unable to seek!");
     let mut output_file = File::create(format!("{}.{}", &filename, ext)).expect("Unable to create output file!");
     std::io::copy(&mut input_file, &mut output_file).expect("Unable to copy data!");
 
+    if let Some(metadata_filename) = metadata_filename_opt {
+        let mut metadata_file = std::fs::OpenOptions::new().create(true).append(true).open(metadata_filename).expect("Unable to open metadata file!");
+        write!(metadata_file, "File: {}\nName: {}\nCreator: {}\n\n", &filename, &name, &creator);
+    }
 }
